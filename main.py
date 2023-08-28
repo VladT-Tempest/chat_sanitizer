d3f921c84cf01999d17186a323963560c2ef2095
Alejandro Aguilera Wilches <aaguile@hotmail.com>
2023-08-28 17:34:37 -0500

    class settings
    Handle loading and accessing application settings from file

--- main.py
+++ main.py
@@ -6,6 +6,34 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+class Settings(dict):
+    """Handle loading and accessing application settings from file."""
+
+    @classmethod
+    def load(cls, path) -> "Settings":
+        """Load tomllib settings file and pass it to class constuctor."""
+        with path.open("rb") as file:
+            return cls(tomllib.load(file))
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Add general settings and prompts as instance attributes."""
+        super().__init__(*args, **kwargs)
+        # Settings
+        self.chat_models = self["general"]["chat_models"]
+        self.model = self["general"]["model"]
+        self.max_tokens = self["general"]["max_tokens"]
+        self.temperature = self["general"]["temperature"]
+        self.model_supports_chat_completions = self.model in self.chat_models
+        # Prompts
+        self.instruction_prompt = self["prompts"]["instruction_prompt"]
+        self.role_prompt = self["prompts"]["role_prompt"]
+        self.positive_example = self["prompts"]["positive_example"]
+        self.positive_reasoning = self["prompts"]["positive_reasoning"]
+        self.positive_output = self["prompts"]["positive_output"]
+        self.negative_example = self["prompts"]["negative_example"]
+        self.negative_reasoning = self["prompts"]["negative_reasoning"]
+        self.negative_output = self["prompts"]["negative_output"]
+
 def main(args: argparse.Namespace) -> None:
     file_content = args.file_path.read_text("utf-8")
     settings = Settings.load(Path("settings.toml"))
